4f733c86979f01cb0937f909844407b33f148b15
lilalr <gurennut@gmail.com>
2024-03-07 19:46:51 +0700

    menambah variabel main

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -25,3 +25,52 @@ void input() {				// procedur untuk input
 	}
 }
 
+void BubbleSortArray() {	// procedur untuk mengurutkan array dengan metode bubble sort
+	int pass = 1;			// step 1
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) {	// step 2
+			if (arr[j] > arr[j + 1]) {	// step 3
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+		pass = pass + 1;	// step 4
+
+		cout << "\nPass " << pass - 2 << ": ";	// Output ke layar
+		for (int k = 0; k < n; k++) {			// looping nilai k dimulai dari 0 hingga n-1
+			cout << arr[k] << " ";				// Output ke layar
+		}
+		cout << endl;
+
+	} while (pass <= n - 1);	// step 5
+}
+
+void display() {
+	cout << endl;
+	cout << "================================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "================================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j];
+		if (j < n - 1) {
+			cout << " --> ";
+
+		}
+	}
+	cout << endl;
+	cout << endl;
+	cout << "Jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
+
+int main() {
+	input(); // call the input function to get user input
+	BubbleSortArray(); // call the selection sort function
+	display(); // display the sortde array
+	system("pause");
+
+	return 0;
+}
